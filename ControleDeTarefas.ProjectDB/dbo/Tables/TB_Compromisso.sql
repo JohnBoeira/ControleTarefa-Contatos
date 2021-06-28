0649b5581f9ea774277bfa7a78a1204eaa5d22f7
johnboeira <jbiel72001@gmail.com>
2021-06-28 18:00:02 -0300

    commit 5

---
+++ ControleDeTarefas.ProjectDB/dbo/Tables/TB_Compromisso.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TB_Compromisso] (
+    [id]          INT          IDENTITY (1, 1) NOT NULL,
+    [assunto]     VARCHAR (50) NOT NULL,
+    [local]       VARCHAR (50) NULL,
+    [dataInicio]  DATETIME     NOT NULL,
+    [dataTermino] DATETIME     NOT NULL,
+    [contato_id]  INT          NULL,
+    [link]        VARCHAR (50) NULL,
+    CONSTRAINT [PK_TB_Compromisso] PRIMARY KEY CLUSTERED ([id] ASC),
+    FOREIGN KEY ([contato_id]) REFERENCES [dbo].[TB_Contatos] ([Id])
+);
+
